75d8b7d389319ef21e598cd6cf61467a0fe98289
Daniil Chumakin <145126478+Pryduck@users.noreply.github.com>
2026-08-19 22:37:12 +0700

    Update payments by cohort.sql

--- payments by cohort.sql
+++ payments by cohort.sql
@@ -1,11 +1,13 @@
 select  *
       ,extract ('day' from ((select max (dtime_pay) from skygame.monetary) - mm )/30) as interv
       ,avg_rev/( extract ('day' from ((select max (dtime_pay) from skygame.monetary) - mm )/30)) avg_rev_per_month
+
 from (
 select    date_trunc('month', reg_date) as mm
         , sum(cnt_buy * price) as revenue
         , count(distinct m.id_user) as cnt
         , sum(cnt_buy * price) / count(distinct m.id_user) as avg_rev
+
 from skygame.monetary m
    join skygame.log_prices p
 	  on m.id_item_buy = p.id_item
@@ -13,6 +15,7 @@ from skygame.monetary m
       and m.dtime_pay <= coalesce(valid_to, to_date('01/01/3000', 'DD/MM/YYYY'))
    join skygame.users u
       on m.id_user = u.id_user
+
 where reg_date < (select max(dtime_pay) - interval '1 month' from skygame.monetary)
 group by mm
 order by mm
